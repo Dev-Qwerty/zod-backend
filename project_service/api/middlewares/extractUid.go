f53cffcec55bee992d486f9ece8faa04e47d95c5
albinj12 <albinavirappatt@gmail.com>
2021-01-20 10:50:52 +0530

    refactor(project_service): save uid in request context

---
+++ project_service/api/middlewares/extractUid.go
@@ -0,0 +1,28 @@
+package middlewares
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/Dev-Qwerty/zod-backend/project_service/api/config"
+)
+
+// type key string
+
+// const tokenUID key = "tokenuid"
+
+// ExtractUID verifies idToken and extracts usertoken
+func ExtractUID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		idToken := r.Header.Get("token")
+		token, err := config.Client.VerifyIDToken(context.TODO(), idToken)
+		if err != nil {
+			fmt.Println(err)
+		}
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, "tokenuid", token.UID)
+		fmt.Println(token)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
