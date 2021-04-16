122f7072e03cea07f867fae877faca72a50acfd7
albinj12 <albinavirappatt@gmail.com>
2021-04-16 11:17:14 +0530

    feat(chat service):create server and initialize db

---
+++ chat_service/src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DBURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .catch((error) => {
+        console.log(error);
+    });
+
+db = mongoose.connection
+
+db.on('open', () => {
+    console.log('Connected to db');
+})
+
+db.on('error', () => {
+    console.log('Cant connect to db');
+})
