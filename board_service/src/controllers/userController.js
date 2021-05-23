8772de22cd08a803f4d950f8a59b0b66d4217d88
amljs <amalavirappatt@gmail.com>
2021-05-23 08:21:28 +0530

    refactor: use http req for addding user to db

--- board_service/src/controllers/userController.js
+++ board_service/src/controllers/userController.js
@@ -1,3 +1,8 @@
+const express = require('express')
+
+const router = express.Router()
+const parseJson = express.json({ extended: true })
+
 // User model
 const User = require('../models/user')
 
@@ -14,4 +19,24 @@ const newUser = (value) => {
         })
 }
 
-module.exports = newUser
+router
+    .route('/new')
+    .post([parseJson], async (req, res) => {
+        try {
+            const { member, projectId } = req.body
+
+            const { name, fid, email, imgUrl, role } = member
+            const projects = {
+                projectId,
+                role
+            }
+
+            const doc = await User.findOneAndUpdate({ email }, { name, fid, email, imgUrl, $push: { projects } }, { upsert: true, new: true })
+            res.status(200).send("Content added")
+        } catch (error) {
+            console.log(error)
+            res.status(500).send(error)
+        }
+    })
+
+module.exports = { newUser, router }
--- board_service/src/routes/route.js
+++ board_service/src/routes/route.js
@@ -1,9 +1,11 @@
 const boardController = require('../controllers/boardController')
+const userController = require('../controllers/userController')
 const isUserVerified = require('../middlewares/verifyUser')
 
 const router = (app) => {
 
     app.use('/api/board', isUserVerified, boardController)
+    app.use('/api/user', userController.router)
     
 }
 
