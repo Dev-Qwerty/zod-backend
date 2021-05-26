c2005eeaaf8bfabbad87d90e408d00efae98efb3
albinj12 <albinavirappatt@gmail.com>
2021-05-26 20:17:29 +0530

    interservice communication routes

--- chat_service/index.js
+++ chat_service/index.js
@@ -17,6 +17,57 @@ const VerifyUser = require('./src/middlewares/verifyuser')
 const app = express()
 const server = http.createServer(app)
 
+
+app.post('/api/channel/everyone/new', [parseJson], async (req, res) => {
+    console.log(req.body)
+    const member = {}
+    const newChannel = new channelModel({
+        projectid: req.body.projectid,
+        channelName: 'everyone',
+        channelid: nanoid(),
+    })
+    member.email = req.body.member.email
+    member.isAdmin = false
+    newChannel.members.push(member)
+    newChannel.save()
+        .then(doc => {
+            console.log(doc)
+            res.end()
+        })
+        .catch(error => {
+            console.log(error)
+            res.end()
+        })
+    const { name, fid, imgUrl, email } = req.body.member
+    projectRole = []
+    projectRole.push({
+        projectid: req.body.projectid,
+        role: req.body.member.role
+    })
+    userModel.findOneAndUpdate({ email }, { name, fid, email, imgUrl, $push: { projectRole } }, { upsert: true, new: true })
+        .then(doc => {
+            console.log(doc)
+            res.end()
+        })
+        .catch(error => {
+            console.log(error)
+            res.end()
+        })
+})
+
+app.post('/api/channel/acceptinvite', [parseJson], async (req, res) => {
+    console.log(req.body)
+    const { name, fid, imgUrl, email } = req.body.member
+    await channelModel.findOneAndUpdate({ projectid: req.body.projectid, channelName: 'everyone' }, { $push: { members: { isAdmin: false, email } } })
+    projectRole = []
+    projectRole.push({
+        projectid: req.body.projectid,
+        role: req.body.member.role
+    })
+    await userModel.findOneAndUpdate({ email }, { name, fid, email, imgUrl, $push: { projectRole } }, { upsert: true, new: true })
+    res.end()
+})
+
 app.use(VerifyUser)
 
 //Initialize Routes
