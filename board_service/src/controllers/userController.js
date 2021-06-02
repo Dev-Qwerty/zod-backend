9f832937c6d1a462129ce9db8bff5d3f4d8f859a
Amal Jose <amaljose197@gmail.com>
2021-06-02 14:06:19 +0530

    minor refactor

--- board_service/src/controllers/userController.js
+++ board_service/src/controllers/userController.js
@@ -8,16 +8,16 @@ const User = require('../models/user')
 
 // Add user details to db
 // When the user creates or accept a project
-const newUser = (value) => {
-    const { name, fid, email, imgUrl, } = value.member
-    const projects = value.projects
-    User.findOneAndUpdate({ email }, { name, fid, email, imgUrl, $push: { projects } }, { upsert: true, new: true })
-        .then(doc => {
-            console.log(doc)
-        }).catch(error => {
-            console.log(error)
-        })
-}
+// const newUser = (value) => {
+//     const { name, fid, email, imgUrl, } = value.member
+//     const projects = value.projects
+//     User.findOneAndUpdate({ email }, { name, fid, email, imgUrl, $push: { projects } }, { upsert: true, new: true })
+//         .then(doc => {
+//             console.log(doc)
+//         }).catch(error => {
+//             console.log(error)
+//         })
+// }
 
 router
     .route('/new')
