b44efeeed7760640dc5962b1fbc093bf729f2965
Amal Jose <amaljose197@gmail.com>
2021-06-06 16:52:33 +0530

    fetch media devices

--- meet/public/script.js
+++ meet/public/script.js
@@ -5,6 +5,18 @@ const peer = new Peer(undefined, {
     port: '8082'
 })
 
+const videoGrid = document.getElementById('video-grid')
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const peers = {}
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then((stream) => {
+    addVideoStream(myVideo, stream)
+})
+
 peer.on('open', (id) => {
     socket.emit('join-room', ROOM_ID, id)
 })
@@ -12,3 +24,11 @@ peer.on('open', (id) => {
 socket.on('user-connected', (userId) => {
     console.log(userId)
 })
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
